d88287e462e14aaf189131b34a22f8aa829a60f1
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-03-13 15:29:47 +0530

    fix BrowserRouter issue

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-application-tracker-backend-1-u0vp.onrender.com",
 });
 
 axiosInstance.interceptors.request.use((config) => {

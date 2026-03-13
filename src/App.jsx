6ab51e0939920aad632c7cfe871a8a5db6de406e
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-03-13 15:24:23 +0530

    fix BrowserRouter issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+// import { Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Dashboard from "./pages/Dashboard";
+// import Profile from "./pages/Profile";
+// import AdminDashboard from "./pages/AdminDashboard";
+// import AdminUsers from "./pages/AdminUsers";
+// import AdminNotifications from "./pages/AdminNotifications";
+// import {ToastContainer} from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/admin" element={<AdminDashboard />} />
+//         <Route path="/admin/users" element={<AdminUsers />} />
+//         <Route path="/admin/notifications" element={<AdminNotifications />} />
+//         {/* <ToastContainer position = "top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/> */}
+//       </Routes>
+//       <ToastContainer position = "top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,11 +36,12 @@ import Profile from "./pages/Profile";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminUsers from "./pages/AdminUsers";
 import AdminNotifications from "./pages/AdminNotifications";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
@@ -20,10 +51,21 @@ function App() {
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/users" element={<AdminUsers />} />
         <Route path="/admin/notifications" element={<AdminNotifications />} />
-        {/* <ToastContainer position = "top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/> */}
       </Routes>
-      <ToastContainer position = "top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>
-    </BrowserRouter>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 }
 

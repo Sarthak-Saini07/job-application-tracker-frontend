61f2b40e7d43fb464e669fdfc19c7e86d491d647
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-03-14 07:44:03 +0530

    cors resolve

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://your-backend.onrender.com/api",
+  baseURL: "https://job-application-tracker-backend-1-u0vp.onrender.com",
 });
 
 axiosInstance.interceptors.request.use((config) => {
@@ -18,3 +18,21 @@ axiosInstance.interceptors.request.use((config) => {
 });
 
 export default axiosInstance;
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://job-application-tracker-backend-1-u0vp.onrender.com/api",
+// });
+
+// axiosInstance.interceptors.request.use((config) => {
+
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default axiosInstance;
